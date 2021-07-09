9f351add42a5ecabf7be7abf621433d689c7792b
Neelesh Pateriya <npateriy@cisco.com>
2021-07-09 16:03:51 -0700

    Update GMSIGconfig.py

--- GMSIGconfig.py
+++ GMSIGconfig.py
@@ -7,14 +7,14 @@ api_key = ''
 
 get_operation_mappings_file = 'API_GET_operations.csv'
 meraki_base_api_url = 'https://api.meraki.com/api/v1/'
-meraki_base_path = '/opt/MerakiGit/orgid'
+meraki_base_path = '/home/developer/MerakiGit/orgid'
 log_path = 'logs'  # Using a subordinate directory from location of Python script called 'logs' - change to suite
 
-git_base_path = '/opt/MerakiGit/orgid'
+git_base_path = '/home/developer/MerakiGit/orgid'
 git_user_email = 'somebody@example.com'
 git_user_name = 'FirstName LastName'
 
-web_publishing_dir = '/var/www/html/DevNetDashboards/MerakiGit'
+web_publishing_dir = '/home/developer/html/DevNetDashboards/MerakiGit'
 web_url = '/DevNetDashboards/MerakiGit'
 
 max_threads = 3
